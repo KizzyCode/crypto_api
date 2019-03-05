c4a8509511a65037f0b494a979be2d3dc67d9c88
KizzyCode <development@kizzycode.de>
2019-03-05 15:35:54 +0100

    Added API to process the data *not* in place

--- src/cipher.rs
+++ src/cipher.rs
@@ -30,10 +30,16 @@ pub trait Cipher: SecKeyGen {
 	/// ciphertext length
 	fn encrypt(&self, buf: &mut[u8], plaintext_len: usize, key: &[u8], nonce: &[u8])
 		-> Result<usize, Box<dyn Error + 'static>>;
+	/// Encrypts `plaintext` into `buf` using `key` and `nonce` and returns the ciphertext length
+	fn encrypt_to(&self, buf: &mut[u8], plaintext: &[u8], key: &[u8], nonce: &[u8])
+		-> Result<usize, Box<dyn Error + 'static>>;
 	
 	/// Decrypts `ciphertext_len` bytes in-place in `buf` using `key` and `nonce` and returns the
 	/// plaintext length
 	fn decrypt(&self, buf: &mut[u8], ciphertext_len: usize, key: &[u8], nonce: &[u8])
+		-> Result<usize, Box<dyn Error + 'static>>;
+	/// Decrypts `ciphertext` into `buf` using `key` and `nonce` and returns the plaintext length
+	fn decrypt_to(&self, buf: &mut[u8], ciphertext: &[u8], key: &[u8], nonce: &[u8])
 		-> Result<usize, Box<dyn Error + 'static>>;
 }
 
@@ -43,10 +49,18 @@ pub trait AeadCipher: Cipher {
 	/// `nonce` and returns the ciphertext length
 	fn seal(&self, buf: &mut[u8], plaintext_len: usize, ad: &[u8], key: &[u8], nonce: &[u8])
 		-> Result<usize, Box<dyn Error + 'static>>;
+	/// AEAD-seals `plaintext` into `buf` together with `ad` using `key` and `nonce` and returns the
+	/// ciphertext length
+	fn seal_to(&self, buf: &mut[u8], plaintext: &[u8], ad: &[u8], key: &[u8], nonce: &[u8])
+		-> Result<usize, Box<dyn Error + 'static>>;
 	
 	/// AEAD-opens `ciphertext_len` bytes in-place in `buf` together with `ad` using `key` and
 	/// `nonce` and returns the plaintext length
 	fn open(&self, buf: &mut[u8], ciphertext_len: usize, ad: &[u8], key: &[u8], nonce: &[u8])
+		-> Result<usize, Box<dyn Error + 'static>>;
+	/// AEAD-opens `ciphertext` into `buf` together with `ad` using `key` and `nonce` and returns
+	/// the plaintext length
+	fn open_to(&self, buf: &mut[u8], ciphertext: &[u8], ad: &[u8], key: &[u8], nonce: &[u8])
 		-> Result<usize, Box<dyn Error + 'static>>;
 }
 
@@ -58,12 +72,10 @@ pub trait StreamingCipher: SecKeyGen {
 	
 	/// (Re-)initializes the handle with `key` and `nonce`
 	fn init(&mut self, key: &[u8], nonce: &[u8]) -> Result<(), Box<dyn Error + 'static>>;
-	
 	/// Processes the bytes in `input` and writes the resulting bytes to `output` and returns the
 	/// amount of bytes written
 	fn update<'a>(&mut self, input: impl Iterator<Item = &'a u8>,
 		output: impl Iterator<Item = &'a mut u8>) -> Result<usize, Box<dyn Error + 'static>>;
-	
 	/// Finishes the operation and writes the pending bytes to `output` and returns the amount of
 	/// bytes written
 	fn finish<'a>(&mut self, output: impl Iterator<Item = &'a mut u8>)
