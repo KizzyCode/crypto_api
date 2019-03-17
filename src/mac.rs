888baa50057f3dc87fed04e774dd3a3b534fea7d
KizzyCode <development@kizzycode.de>
2019-03-17 15:24:11 +0100

    Reworked MAC to implement a Varlen API

--- src/mac.rs
+++ src/mac.rs
@@ -11,6 +11,8 @@ pub struct MacInfo {
 	/// Indicates if this MAC is a one time MAC (= requires a unique key for each message)
 	pub is_otm: bool,
 	
+	/// The default/selected MAC length
+	pub mac_len: usize,
 	/// The supported MAC lengths
 	pub mac_len_r: Range<usize>,
 	/// The supported key lengths
@@ -24,7 +26,14 @@ pub trait Mac: SecKeyGen {
 	fn info(&self) -> MacInfo;
 	
 	/// Authenticates `data` into `buf` using `key` and returns the MAC length
-	fn authenticate(&self, buf: &mut[u8], data: &[u8], key: &[u8])
+	fn auth(&self, buf: &mut[u8], data: &[u8], key: &[u8])
+		-> Result<usize, Box<dyn Error + 'static>>;
+}
+
+/// A variable length extension for `Mac`
+pub trait VarlenMac: Mac {
+	/// Authenticates `data` into `buf` using `key` and returns the MAC length
+	fn varlen_auth(&self, buf: &mut[u8], data: &[u8], key: &[u8])
 		-> Result<usize, Box<dyn Error + 'static>>;
 }
 
@@ -40,4 +49,10 @@ pub trait StreamingMac: SecKeyGen {
 	fn update(&mut self, data: &[u8]) -> Result<(), Box<dyn Error + 'static>>;
 	/// Computes the MAC into `buf` and returns the MAC length
 	fn finish(&mut self, buf: &mut[u8]) -> Result<usize, Box<dyn Error + 'static>>;
+}
+
+/// A variable length extension for `StreamingMac`
+pub trait VarlenStreamingMac: StreamingMac {
+	/// (Re-)initializes the MAC state with `key` to produce an `mac_len`-sized hash
+	fn varlen_init(&mut self, mac_len: usize, key: &[u8]) -> Result<(), Box<dyn Error + 'static>>;
 }
